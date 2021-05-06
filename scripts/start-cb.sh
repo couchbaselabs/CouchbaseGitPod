b9edd26dbaf87acf6a2edec3e0cc8b276826da92
Denis Rosa <denisrosa@MacBook-Pro.local>
2021-05-06 12:34:45 +0200

    removing line from start-cb

--- scripts/start-cb.sh
+++ scripts/start-cb.sh
@@ -3,4 +3,3 @@ exec /opt/couchbase/bin/couchbase-server -- -kernel global_enable_tracing false 
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8091)" != "301" ]]; do sleep 5; done
 cd /opt/couchbase/bin
 ./couchbase-cli cluster-init -c localhost --cluster-username Administrator --cluster-password password --services data,index,query,fts,analytics,eventing --cluster-ramsize 4096
-/bin/bash
